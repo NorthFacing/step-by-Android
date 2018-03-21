f401767ef9fcb9e6e1776b027aeebd3096dd79cd
Bob <0haizhu0@gmail.com>
2018-03-21 23:57:38 +0800

    第一行代码：复写菜单处理

--- 02-ActitvityTest/app/src/main/java/com/adolphor/a02_actitvitytest/FirstActivity.java
+++ 02-ActitvityTest/app/src/main/java/com/adolphor/a02_actitvitytest/FirstActivity.java
@@ -2,6 +2,8 @@ package com.adolphor.a02_actitvitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,11 +21,30 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Toast事件
-                Toast.makeText(FirstActivity.this, "You clicked Button 1",
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
+    // 复写菜单：return true显示，false 不显示
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    // 复写菜单点击事件
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(FirstActivity.this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(FirstActivity.this, "Yout clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default: // 可以省略！
+        }
+        return true;
     }
 }
