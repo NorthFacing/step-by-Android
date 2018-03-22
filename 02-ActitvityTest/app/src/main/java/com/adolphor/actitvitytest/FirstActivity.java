e67ee57ff1d6df37ff5d5878b9c98c4db3161000
Bob <0haizhu0@gmail.com>
2018-03-22 23:39:52 +0800

    第一行代码：【activity】:open_mouth:隐式Intent:open_mouth: 访问网页 & 拨打电话

--- 02-ActitvityTest/app/src/main/java/com/adolphor/actitvitytest/FirstActivity.java
+++ 02-ActitvityTest/app/src/main/java/com/adolphor/actitvitytest/FirstActivity.java
@@ -1,6 +1,7 @@
 package com.adolphor.actitvitytest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -54,6 +55,28 @@ public class FirstActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent("com.adolphor.activitytest.ACTION_START");
                 intent.addCategory("com.adolphor.activitytest.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+
+        // 隐式Intent：调用系统组件
+        Button button_web_view = (Button) findViewById(R.id.button_intent_web_view);
+        button_web_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+
+        // 隐式Intent：拨打电话
+        Button button_tel = (Button) findViewById(R.id.button_tel);
+        button_tel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
                 startActivity(intent);
             }
         });
